5320c406c49ae9b6bfecf3473aa38a69e67d426a
Saransh <saransh@Saranshs-MacBook-Pro.local>
2019-11-12 10:13:25 +0530

    sorting done

---
+++ datastructures/sorting/quick_sort.py
@@ -0,0 +1,23 @@
+def partition(arr,start,end):
+    i=start-1 #if oth ele needed to swap i shud be -1,then i will inc later become 0
+    pivot = arr[end]
+    for x in range(start,end):
+        if arr[x]<pivot:
+            i+=1        #because we need to return i of swap happned not i after swap
+            arr[x],arr[i]=arr[i],arr[x]
+    arr[i+1],arr[end] = arr[end],arr[i+1]
+    return i+1  #swapped position of i
+
+
+def quicksort(arr,start,end):
+    if start<end:
+        p = partition(arr,start,end)
+        quicksort(arr,start,p-1) # pivot left ele
+        quicksort(arr,p+1,end) # pivot right ele
+
+
+
+
+arr=[14,8,12,6,4,9,13]
+quicksort(arr,0,len(arr)-1)
+print(arr)
