7855f3215ea7cd2d7e17c9cf105bda57bdfacb60
Saransh <saransh@Saranshs-MacBook-Pro.local>
2019-11-19 16:43:50 +0530

    bfs done

---
+++ datastructures/graph/breadth_first_search.py
@@ -0,0 +1,32 @@
+class Graph: 
+	def __init__(self,v): 
+		self.graph = [None]*v
+	def addEdge(self,u,v): 
+		if self.graph[u] is not None:
+			self.graph[u].append(v) 
+		else:
+			self.graph[u]=[]
+			self.graph[u].append(v)
+	def BFS(self, s): 
+		visited = [False] * (len(self.graph)) 
+		queue = [] 
+		queue.append(s) 
+		visited[s] = True
+		while queue: 
+			s = queue.pop(0) 
+			print (s, end = " ") 
+			for i in self.graph[s]: 
+				if visited[i] == False: 
+					queue.append(i) 
+					visited[i] = True
+g = Graph(4) 
+g.addEdge(0, 1) 
+g.addEdge(0, 2) 
+g.addEdge(1, 2) 
+g.addEdge(2, 0) 
+g.addEdge(2, 3) 
+g.addEdge(3, 3) 
+print ("Following is Breadth First Traversal (starting from vertex 2)") 
+g.BFS(2) 
+
+#Basically level wise node traversal imagine graph as tree then level wise traversal
---
+++ datastructures/graph/graph_by_adjacency_list.py
@@ -0,0 +1,61 @@
+class AdjNode: 
+    def __init__(self, data): 
+        self.vertex = data 
+        self.next = None
+
+class Graph: 
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.graph = [None] * self.V
+  
+    def add_edge(self, src, dest): 
+        
+        #undirected graph
+        #made node of dest attached to src
+        node = AdjNode(dest) 
+        node.next = self.graph[src] 
+        self.graph[src] = node 
+  
+        #made node of src attached to dest
+        node = AdjNode(src) 
+        node.next = self.graph[dest] 
+        self.graph[dest] = node 
+  
+    def print_graph(self): 
+        for i in range(self.V): 
+            print("Adjacency list of vertex {}\n head".format(i), end="") 
+            temp = self.graph[i] 
+            while temp: 
+                print(" -> {}".format(temp.vertex), end="") 
+                temp = temp.next
+            print(" \n") 
+  
+  
+graph = Graph(5) 
+graph.add_edge(0, 1) 
+graph.add_edge(0, 4) 
+graph.add_edge(1, 2) 
+graph.add_edge(1, 3) 
+graph.add_edge(1, 4) 
+graph.add_edge(2, 3) 
+graph.add_edge(3, 4) 
+
+graph.print_graph() 
+
+"""
+Adjacency list of vertex 0
+ head -> 4 -> 1 
+
+Adjacency list of vertex 1
+ head -> 4 -> 3 -> 2 -> 0 
+
+Adjacency list of vertex 2
+ head -> 3 -> 1 
+
+Adjacency list of vertex 3
+ head -> 4 -> 2 -> 1 
+
+Adjacency list of vertex 4
+ head -> 3 -> 1 -> 0
+ 
+"""
