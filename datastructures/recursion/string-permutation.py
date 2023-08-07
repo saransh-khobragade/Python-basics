67fac615754ac81cb716f76e636715c12ff9e105
saransh-khobragade <saransh98@gmail.com>
2023-08-07 21:22:28 +0530

    string permutation done

--- datastructures/recursion/string-permutation.py
+++ datastructures/recursion/string-permutation.py
@@ -1,10 +1,16 @@
-def permute(s):
-    out = []
-    if len(s) == 1:
-        return [s]
+def permutation(arr,pos,size):
+    result=[]
+    if pos == size:
+        return ["".join(arr)]
     else:
-        for i,v in enumerate(s):
-            for x in permute(s[:i]+s[i+1:]):
-                out+=[v+x]
-    return out
-print((permute('abc')))
+        for i in range(pos,size):
+            arr[i],arr[pos] = arr[pos],arr[i]
+            output = permutation(arr,pos+1,size)
+            result.extend(output)
+            arr[i],arr[pos] = arr[pos],arr[i]
+    return result
+
+arr = ["A","B","C"]
+size = len(arr)
+
+print(permutation(arr,0,size))
