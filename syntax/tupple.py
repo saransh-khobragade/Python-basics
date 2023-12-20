e29b9fe7ad443a6dcbed8777cf2c7392955dbfa5
Saransh.K <saransh98@gmail.com>
2023-12-20 11:14:58 +0530

    done

---
+++ syntax/tupple.py
@@ -0,0 +1,2 @@
+a = ("apple","banna","orange")
+print(a)
--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-d = {}
-d[("a","b","c")]=1
-print(d)
-
+from typing import List
+from collections import defaultdict
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        mp=defaultdict(list)
+        bl=defaultdict(bool)
+        mx=0
+        for i in nums:
+            if bl[i]:
+                continue 
+            bl[i]=True
+            l,r=i,i
+            if mp[i+1]:
+                r=mp[i+1][0]
+            if mp[i-1]:
+                l=mp[i-1][1]
+            mp[r]=[r,l]
+            mp[l]=[r,l]
+            mx=max(mx,r-l+1)
+        return mx
+s = Solution()
+s.longestConsecutive([100,4,200,1,3,2])
